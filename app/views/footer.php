aeb3de86ac6408a9e186a0f455054ec99f74cc3d
Pierre Bouvier <pierre@prieuretaluyers.fr>
2017-10-06 15:19:20 +0200

    Addind footer.php

---
+++ app/views/footer.php
@@ -0,0 +1,18 @@
+<footer class="page-footer indigo lighten-3">
+    <div class="container">
+        <div class="row center">
+            <div class="col s12 m6 l2"><a href="#" class="grey-text text-darken-4">Accueil</a></div>
+            <div class="col s12 m6 l4"><a href="#" class="grey-text text-darken-4">Logiciels Marketing Automation</a></div>
+            <div class="col s12 m6 l4"><a href="#" class="grey-text text-darken-4">Comparatif Logiciels Marketing</a></div>
+            <div class="col s12 m6 l2"><a href="#" class="grey-text text-darken-4">A propos</a></div>
+        </div>
+    </div>
+    <div class="footer-copyright">
+        <div class="container">
+            <div class="row">
+                © 2017 Pierre Ammeloot
+                <a class="grey-text text-lighten-4 right" href="#">Plus de liens</a>
+            </div>
+        </div>
+    </div>
+</footer>
