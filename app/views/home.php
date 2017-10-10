b5cd55349625bad96dc727263aba3e864c8d9a92
Funny Rose <fannyperretlombard@gmail.com>
2017-10-10 15:15:14 +0200

    New HomePage

---
+++ app/views/home.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+
+    <link rel="stylesheet" href="#">
+
+
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+</html>
+<body>
+
+<div class="center-align">
+    <div class="row">
+        <div class="col s12 m8 offset-m2 l6 offset-l3">
+                <h1 class="search_bar">Homepage</h1>
+                <a href="https://placeholder.com"><img src="http://via.placeholder.com/350x150"></a>
+        </div>
+    </div>
+</div>
+
+<div class="center-align">
+    <div class="row">
+        <div class="col s12 m8 offset-m2 l6 offset-l3">
+            <div class="card-panel grey lighten-5 z-depth-1">
+                <form id="search-site" action='search' method='get'>
+                    <div class="input-group">
+                        <div class="input-field">
+                            <input id="search" type="search" name='search' placeholder="Ecrivez ici votre recherche...">
+                            <a class="waves-effect waves-light btn-large"><i class="large material-icons right">search</i>Rechercher</a>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+
+<!-- Compiled and minified JavaScript -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</body>
