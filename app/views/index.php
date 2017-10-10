35923163fd399406c4529bae11007b7ca10d0594
al2b <deboissieu.al@gmail.com>
2017-10-10 14:11:22 +0200

    Revert "Navbar"

--- app/views/index.php
+++ app/views/index.php
@@ -5,28 +5,3 @@
  * Date: 04/10/17
  * Time: 15:08
  */
-
-
-?>
-
-<!doctype html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport"
-          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-
-    <!-- Latest compiled and minified CSS & JS -->
-    <link rel="stylesheet" media="screen" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-
-</head>
-<body>
-
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
-<script src="../../web/js/main.js"></script>
-
-</body>
-</html>
