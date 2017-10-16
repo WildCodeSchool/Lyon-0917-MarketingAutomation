af1a7ae1508a045a23e01c4107c0ba5ba63c1013
Funny Rose <fannyperretlombard@gmail.com>
2017-10-16 18:08:32 +0200

    New file listing

---
+++ app/views/listing.php
@@ -0,0 +1,349 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: fannyperret
+ * Date: 16/10/2017
+ * Time: 15:10
+ */ ?>
+
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- Compiled and minified CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="#">
+    <title>Listing_page</title>
+</head>
+<body>
+
+<!-- Navbar goes here -->
+
+<!-- Page Layout here -->
+<div class="row">
+    <div class="valign-wrapper">
+        <div class="grid-example col s12 m4 l3"></div>
+        <div class="col s12 m8 l9">
+            <div class ="title">
+                <h1>Page Listing Logiciels</h1>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+              <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2 m8 l11">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10 m4 l1">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+<div class="col s12 m6 l3">
+    <div class="card-panel grey lighten-5 z-depth-1">
+        <div class="row valign-wrapper">
+            <div class="col s2">
+                <img src="https://www.acd-groupe.fr/galerie/produits/logo_expert_comptabilite.png" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+            </div>
+            <div class="col s10">
+                <h2>Logiciel_name</h2>
+                <span class="black-text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+              </span>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
+</body>
+</html>
