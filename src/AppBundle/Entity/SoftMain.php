f6fd4eddd35762367bd1ffe71d746c7d1359f981
al2b <deboissieu.al@gmail.com>
2017-11-21 15:56:51 +0100

    add relations one to one unidirectionnal with other part of Software entities

--- src/AppBundle/Entity/SoftMain.php
+++ src/AppBundle/Entity/SoftMain.php
@@ -84,6 +84,79 @@ class SoftMain
      */
     private $versus2;
 
+    /**
+     * One MainSoftware has One SoftInfo.
+     * @ORM\OneToOne(targetEntity="SoftInfo")
+     * @ORM\JoinColumn(name="SoftInfo_id", referencedColumnName="id")
+     */
+
+    private $SoftInfo;
+
+    /**
+     * One MainSoftware has One SoftOutbound.
+     * @ORM\OneToOne(targetEntity="SoftOutbound")
+     * @ORM\JoinColumn(name="SoftOutbound_id", referencedColumnName="id")
+     */
+
+    private $SoftOutbound;
+
+    /**
+     * One MainSoftware has One SoftCommSupport.
+     * @ORM\OneToOne(targetEntity="SoftCommSupport")
+     * @ORM\JoinColumn(name="SoftCommSupport_id", referencedColumnName="id")
+     */
+    private $SoftCommSupport;
+
+    /**
+     * One MainSoftware has One SoftCommSupport.
+     * @ORM\OneToOne(targetEntity="SoftLeadsOperation")
+     * @ORM\JoinColumn(name="SoftLeadsOperation_id", referencedColumnName="id")
+     */
+    private $SoftLeadsOperation;
+
+    /**
+     * One MainSoftware has One SoftSegmentOperation.
+     * @ORM\OneToOne(targetEntity="SoftSegmentOperation")
+     * @ORM\JoinColumn(name="SoftSegmentOperation_id", referencedColumnName="id")
+     */
+    private $SoftSegmentOperation;
+
+    /**
+     * One MainSoftware has One SoftMarketingCampaign.
+     * @ORM\OneToOne(targetEntity="SoftMarketingCampaign")
+     * @ORM\JoinColumn(name="SoftMarketingCampaign_id", referencedColumnName="id")
+     */
+    private $SoftMarketingCampaign;
+
+    /**
+     * One MainSoftware has One SoftSocialMedia.
+     * @ORM\OneToOne(targetEntity="SoftSocialMedia")
+     * @ORM\JoinColumn(name="SoftSocialMedia_id", referencedColumnName="id")
+     */
+    private $SoftSocialMedia;
+
+    /**
+     * One MainSoftware has One SoftReport.
+     * @ORM\OneToOne(targetEntity="SoftReport")
+     * @ORM\JoinColumn(name="SoftReport_id", referencedColumnName="id")
+     */
+    private $SoftReport;
+
+    /**
+     * One MainSoftware has One SoftSupport.
+     * @ORM\OneToOne(targetEntity="SoftSupport")
+     * @ORM\JoinColumn(name="SoftSupport_id", referencedColumnName="id")
+     */
+    private $SoftSupport;
+
+    /**
+     * One MainSoftware has One SoftOtherFunctionnalities.
+     * @ORM\OneToOne(targetEntity="SoftOtherFunctionnalities")
+     * @ORM\JoinColumn(name="SoftOtherFunctionnalities_id", referencedColumnName="id")
+     */
+    private $SoftOtherFunctionnalities;
+
+
 
     /**
      * Get id
