c74d230956c9d74cbd32b2f3410b9b0b7a2f69be
Pierrick Reux <pierrick.reux@gmail.com>
2017-11-27 15:46:31 +0100

    importTag added, yml beggined,splFile used, methods planned

--- src/AppBundle/Command/ImportCommand.php
+++ src/AppBundle/Command/ImportCommand.php
@@ -3,13 +3,13 @@
 namespace AppBundle\Command;
 
 
-use AppBundle\Entity\Tag;
+
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
-use AppBundle\Service\Slugification;
+
 
 class ImportCommand extends ContainerAwareCommand
 
@@ -41,39 +41,23 @@ class ImportCommand extends ContainerAwareCommand
         // Here, we need to get Input Argument. We have to catch 3 arguments.
         $inputFileTags = $input->getArgument('filetags');
 
-        // To do : Check if this is really csv in good format. If not, threw exception. Because we need 3 good csv to work.
+        $importTag = $this->getContainer()->get('app.import');
+        $importTag->importTags($inputFileTags);
 
-        // If file is okay : transform in splFileObject
-        $fileTags = new \SplFileObject($inputFileTags);
-        // Ready to be read and skip empty row
-        $fileTags->setFlags(\SplFileObject::READ_CSV | \SplFileObject::READ_AHEAD | \SplFileObject::SKIP_EMPTY | \SplFileObject::DROP_NEW_LINE);
+
+
+        // To do : Check if this is really csv in good format. If not, threw exception. Because we need 3 good csv to work.
 
         // To do : open transaction.
         //$this->getContainer()->
 
         // Here the foreach to hydrate entity Tags. Only one verification : if the name already exists.
         // To do : exclude header and verify data.
-        $slugificator = $this->getContainer()->get('app.slug');
 
-        while (!$fileTags->eof()) {
-            foreach ($fileTags as $row) {
-                list($name, $description) = $row;
-                $tag = $this->em->getRepository(Tag::class)
-                    ->findOneBy([
-                        'name' => $name,
-                    ]);
-                if(null === $tag) {
-                    $tag = new Tag();
-                    $tag->setName($name);
-                    $tag->setDescription($description);
-                    $slug = $slugificator->slugFactory($name);
-                    $tag->setSlug($slug);
-                    $this->em->persist($tag);
-                    $this->em->flush();
-                }
 
-            }
-        }
+      //  $slugificator = $this->getContainer()->get('app.slug');
+
+
 
         // End of transaction and commit if already went good.
 
--- src/AppBundle/Service/ImportEntities.php
+++ src/AppBundle/Service/ImportEntities.php
@@ -2,96 +2,154 @@
 
 namespace AppBundle\Service;
 
+use AppBundle\Entity\Tag;
 use Doctrine\Common\Persistence\ObjectManager;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Output\OutputInterface;
 
 class ImportEntities
 {
 // Première version : on hydrate nos objets manuellement
 // Voir une v2 où on pourrait créer les entités à la volée, ce qui permettrait une meilleure maintenance du système et la possibilité de maintenir la base (MAJ des colonnes, par exemple)
 
-    /** @var ObjectManager  */
+    /** @var ObjectManager */
     private $em;
-    /** @var \SplFileObject  */
-    private $file;
-    private $totalLines;
+    private $slugificator;
+    private $errors;
 
-    public function __construct(ObjectManager $em, $fileFromConsole)
+    public function __construct(ObjectManager $em, Slugification $slugificator)
     {
-        /*
+        $this->slugificator = $slugificator;
         $this->em = $em;
+        $this->errors = array();
+        /*
+                // D'abord on vérifie si ce qu'on reçoit est bien un fichier lisible, si c'est pas le cas, on envoie une exception
+                if(!file_exists($fileFromConsole)) {
+                    //TODO : améliorer ce truc
+                    throw new \Exception("Ce n'est pas un fichier");
+                }else{
 
-        // D'abord on vérifie si ce qu'on reçoit est bien un fichier lisible, si c'est pas le cas, on envoie une exception
-        if(!file_exists($fileFromConsole)) {
-            //TODO : améliorer ce truc
-            throw new \Exception("Ce n'est pas un fichier");
-        }else{
 
-            // Si c'est un fichier, on créé une nouvelle instance de SplFileObject
-        $this->file = new \SplFileObject($fileFromConsole);
 
-        // On définit les drapeaux : saute la ligne si elle est vide
-        $this->file->setFlags(\SplFileObject::READ_AHEAD |
-            \SplFileObject::SKIP_EMPTY |
-            \SplFileObject::DROP_NEW_LINE);
-
-        // On va à la fin max du fichier
-        $this->file->seek(PHP_INT_MAX);
-
-        // On récupère le nombre de lignes et on hydrate la propriété avec
-        $this->totalLines = $this->file->key() + 1;
-
-        // On remonte le pointeur au début du fichier
-        $this->file->seek(0);
-
-        }
-         */
+                }
+                 */
     }
 
     public function checkIfString()
     {
         //Check if data is string, if it's not, return error
         // But I don't know if it's really efficient (utf8?)
+        // Vérifier que le texte n'est pas "oui" ou "non"
     }
 
-    public function checkIfInteger()
+    public function checkIfInteger($value, $column)
     {
-        //Check if data is integer, if it's not, return error
+        if (is_int($value) === FALSE) {
+            array_push($this->errors, "Column" . $column . ": " . $value . "is expected to be an integer");
+        }
     }
 
-    public function checkIfBool()
+    //si c'est différent de oui ou non, mais que c'est quand meme set ->error)
+    public function checkIfBool($value, $column)
     {
-        //Check if data is oui or non or null (if empty), if it's not, return error
-    }
-
-
-    public function importTags($data)
-    {
-        // From csv, check if data is correct, if is not, return error
-
-        // Getting php array of data from CSV with method get (to do)
-        /*
-        $csv = $this->file->fgetcsv(",");
-        for ($i = 1; $this->totalLines, $i++){
-            if($i===1){
-                foreach($csv)
+        if ($value != "oui" || $value != "non") {
+            if (isset($value)) {
+                array_push($this->errors, "Column" . $column . ": " . $value . "is expected to be a boolean");
             }
         }
-        */
+
+    }
+
+// conversion des "oui" ou "non" ou "" par les booleens correspondants
+    public function convertToBool($value)
+    {
+        switch ($value) {
+            case "oui":
+                return $value = TRUE;
+                break;
+            case "non":
+                return $value = FALSE;
+                break;
+            case (!isset($value)):
+                return $value = FALSE;
+                break;
+        }
+    }
+
+
+    public function importTags($fileTags)
+    {
+        // A tester sans le while
+        $splFileTags = $this->fileInit($fileTags);
+        $totalLines = $this->countLines($splFileTags);
+        while (!$splFileTags->eof()) {
+            foreach ($splFileTags as $row) {
+                list($name, $description) = $row;
+                $tag = $this->em->getRepository(Tag::class)
+                    ->findOneBy([
+                        'name' => $name,
+                    ]);
+                if (null === $tag) {
+                    $tag = new Tag();
+                    $tag->setName($name);
+                    $tag->setDescription($description);
+
+                    $slug = $this->slugificator->slugFactory($name);
+                    $tag->setSlug($slug);
+                    $this->em->persist($tag);
+                    $this->em->flush();
+                }
+
+            }
+        }
     }
 
     public function importSoftware($data)
     {
+        $this->fileInit($data);
         // From csv, check if data is correct, if is not, return error
 
     }
 
     public function importVersus($data)
     {
+        $this->fileInit($data);
         // From csv, check if data is correct, if is not, return error
 
     }
 
+    public function getErrors()
+    {
+        return $this->errors;
+    }
 
+    private function fileInit($fileFromConsole)
+    {
+        //default delimiter is ","
+
+        // Si c'est un fichier, on créé une nouvelle instance de SplFileObject
+        $file = new \SplFileObject($fileFromConsole);
+
+        // On définit les drapeaux : saute la ligne si elle est vide
+
+        $file->setFlags(
+            \SplFileObject::READ_CSV |
+            \SplFileObject::READ_AHEAD |
+            \SplFileObject::SKIP_EMPTY |
+            \SplFileObject::DROP_NEW_LINE);
+
+        return $file;
+    }
+
+    private function countLines(\SplFileObject $file){
+
+        // On va à la fin max du fichier
+        $file->seek(PHP_INT_MAX);
+
+        // On récupère le nombre de lignes et on hydrate la propriété avec
+        $totalLines = $file->key() + 1;
+
+        // On remonte le pointeur au début du fichier
+        $file->seek(0);
+
+        return $totalLines;
+    }
 }
