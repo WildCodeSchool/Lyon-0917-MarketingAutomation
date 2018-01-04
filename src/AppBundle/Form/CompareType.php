33e0e8fa892090670449511c50c81b407cfe356e
Pierrick Reux <pierrick.reux@gmail.com>
2018-01-04 12:00:29 +0100

    add placeholder direct in form options

--- src/AppBundle/Form/CompareType.php
+++ src/AppBundle/Form/CompareType.php
@@ -7,38 +7,31 @@ use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Validator\Constraints\NotBlank;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\HttpFoundation\Session\Session;
 
 
 class CompareType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $session = new Session();
-        if ($session->get('versus1') != null){
-
-            $placeholder1 = $session->get('versus1');
-        }else{
-            $placeholder1 = '';
+        dump($options);
+        $placeholder1 = "";
+        $placeholder2 = "";
+        if (isset($options['data']['placeholder1'])){
+            $placeholder1 = $options['data']['placeholder1'];
         }
-        if ($session->get('versus2') != null){
-            $placeholder2 = $session->get('versus2');
-        }else{
-            $placeholder2 = '';
+        if (isset($options['data']['placeholder2'])){
+            $placeholder2 = $options['data']['placeholder2'];
         }
-        $session->invalidate();
         $builder
             ->add('software1', TextType::class, array(
                 'label' => 'Choisir le premier logiciel :',
                 'attr' => array('autocomplete' => 'off', 'placeholder' => $placeholder1),
                 'constraints' => array(new NotBlank()),
-
             ))
             ->add('software2', TextType::class, array(
                 'label' => 'Choisir le deuxième logiciel :',
                 'attr' => array('autocomplete' => 'off','placeholder' => $placeholder2),
                 'constraints' => array(new NotBlank()),
-
             ))
             ->getForm();
     }
