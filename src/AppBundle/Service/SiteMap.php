3f0ed6bd1a0c2a303e8c75102ff6a5e185512794
al2b <deboissieu.al@gmail.com>
2018-01-09 18:17:47 +0100

    add versus links, alphabetic order by slug to avoid duplicate

--- src/AppBundle/Service/SiteMap.php
+++ src/AppBundle/Service/SiteMap.php
@@ -5,9 +5,8 @@ namespace AppBundle\Service;
 use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Bundle\FrameworkBundle\Routing\Router;
 use Symfony\Component\Routing\RouterInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use AppBundle\Controller\DefaultController;
 use AppBundle\Entity\Tag;
+use AppBundle\Service\BoolsAsTags;
 
 
 class SiteMap
@@ -15,17 +14,31 @@ class SiteMap
 
     /** @var Router  */
     private $router;
+    /**
+     * @var ObjectManager
+     */
     private $em;
+    /**
+     * @var BoolsAsTags
+     */
+    private $boolsAsTags;
 
-    public function __construct(RouterInterface $router, ObjectManager $em)
+    /**
+     * SiteMap constructor.
+     * @param RouterInterface $router
+     * @param ObjectManager $em
+     * @param \AppBundle\Service\BoolsAsTags $boolsAsTags
+     */
+
+    public function __construct(RouterInterface $router, ObjectManager $em, BoolsAsTags $boolsAsTags)
     {
         $this->router = $router;
         $this->em = $em;
-
+        $this->boolsAsTags = $boolsAsTags;
     }
 
     /**
-     * Generate all routes of website list in controller
+     * Generate all routes of website list in controller and in database
      *
      * @return array
      */
@@ -33,6 +46,7 @@ class SiteMap
 
 
     {
+        // Add softwares to list of urls
         $softwares = $this->em->getRepository('AppBundle:SoftMain')->findAll();
         $urls = [];
 
@@ -42,11 +56,30 @@ class SiteMap
             );
         }
 
+        // Add tags to list of urls
         $tags = $this->em->getRepository(Tag::class)->findAll();
         foreach($tags as $tag) {
             $urls[] = array(
                 'loc' => $this->router->generate('tagSolo', array('slug' => $tag->getSlug()), true)
             );
+        }
+
+        // Add booleans to list of urls
+        $bools = $this->boolsAsTags->getGoodBools();
+        foreach($bools as $bool) {
+            $urls[] = array('loc' => $this->router->generate('tagSolo', array('slug' => $bool['slug']), true));
+        }
+
+        // Add versus to lis of urls
+
+        $versus = $this->em->getRepository("AppBundle:Versus")->findAll();
+        $versusLight = array();
+        foreach($versus as $versu) {
+            $versusLight[] = array($versu->getSoftware1()->getSlug(), $versu->getSoftware2()->getSlug());
+        }
+        foreach($versusLight as $each) {
+            sort($each);
+            $urls[] = array('loc' => $this->router->generate('versus', array('slug1' => $each[0], 'slug2' => $each[1]), true));
         }
 
         $routes = $this->router->getRouteCollection()->all();
