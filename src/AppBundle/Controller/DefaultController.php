156be614a1a0f81f2b8311a680045123077efb95
Pierrick Reux <pierrick.reux@gmail.com>
2018-01-15 15:33:39 +0100

    json not displayed when rollback on page, number of results after filter displayed

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -41,7 +41,7 @@ class DefaultController extends Controller
         $repository = $this->getDoctrine()->getRepository(SoftMain::class);
 
         $softMains = $seeAlso->getListOfSameSoftwares($softMain, 6);
-        $versusList = $this->getDoctrine()->getRepository(Versus::class)->findVersusByOneSoftware($softMain); 
+        $versusList = $this->getDoctrine()->getRepository(Versus::class)->findVersusByOneSoftware($softMain);
 
         return $this->render('default/software.html.twig', [
             'softmain' => $softMain,
@@ -74,20 +74,33 @@ class DefaultController extends Controller
     public function resultsAction(Request $request, $researchContent = "", AwesomeSearch $awesomeSearch)
     {
         $this->get("session")->set("researchContent", $researchContent);
-        $test = array('name' => "pierrick", 'slug' => "pireux");
+
+        return $this->render('default/results.html.twig', [
+            'research' => $researchContent,
+
+        ]);
+
+    }
+
+    /**
+     * @param Request $request
+     * @param $researchContent
+     * @return JsonResponse
+     * @Route("resultsJson_{researchContent}", name="resultsJson", defaults={"researchContent": ""})
+     */
+
+    public function resultsJsonAction(Request $request, $researchContent = "", AwesomeSearch $awesomeSearch)
+    {
         $softwares = $awesomeSearch->search($researchContent);
         if ($request->isXmlHttpRequest()) {
 
             return new JsonResponse(array('data' => $softwares));
-
-
         } else {
-            return $this->render('default/results.html.twig', [
-                'research' => $researchContent,
-                'softwares' => $softwares
-            ]);
+            throw new HttpException('500', 'Invalid call');
         }
     }
+
+
     /**
      * @Route("listing-tags", name="listingTags")
      * @param Request $request
@@ -101,7 +114,7 @@ class DefaultController extends Controller
         $repository = $this->getDoctrine()->getRepository(Tag::class);
         $tags = $repository->findAll();
 
-        foreach($tags as $tag) {
+        foreach ($tags as $tag) {
             $bools[] = array('slug' => $tag->getSlug(), 'number' => count($tag->getSoftMains()), 'entitie' => $tag->getName());
         }
 
@@ -164,7 +177,8 @@ class DefaultController extends Controller
         ]);
     }
 
-    private static function compare(Versus $a, Versus $b) {
+    private static function compare(Versus $a, Versus $b)
+    {
         return strcmp($a->getSoftware1()->getName(), $b->getSoftware1()->getName());
 
     }
@@ -250,7 +264,6 @@ class DefaultController extends Controller
         sort($canonical);
 
         $em = $this->getDoctrine()->getManager();
-
 
 
         $softmain1 = $em->getRepository('AppBundle:SoftMain')->findOneBy([
